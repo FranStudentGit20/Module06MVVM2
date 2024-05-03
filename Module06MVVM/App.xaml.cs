c68acd8f3dcb8ed8f345b85587609d316b52950e
Clavory <clavoryg@gmail.com>
2024-05-03 11:35:45 +0800

    Add project files.

---
+++ Module06MVVM/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http.Headers;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Module06MVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new View.EmployeeView());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Module06MVVM/Model/EmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module06MVVM.Model
+{
+    internal class EmployeeModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+
+    }
+}
